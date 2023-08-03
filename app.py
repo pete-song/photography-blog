7ec514f8db82f5aecbbc7ca0e8b02cabc0f83df0
pete-song <pattapon.pete@gmail.com>
2023-08-03 20:43:39 +0700

    Added Admin page to upload posts to static directory

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask, render_template, request, redirect, flash, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from flask_mysqldb import MySQL
 import yaml
 
+UPLOAD_FOLDER = 'static/image-post'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'crp', 'txt'}
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Never push this line to github public repo"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 cred = yaml.load(open('cred.yaml'), Loader=yaml.Loader)
 app.config['MYSQL_HOST'] = cred['mysql_host']
@@ -19,9 +24,40 @@ mysql = MySQL(app)
 def index():
     return render_template('index.html')
 
+@app.route('/gallery', methods=['GET', 'POST'])
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "error: no selected file"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # ProcessImage
+            return render_template('gallery.html')
+    return render_template('admin/admin.html')
+
+
 
 
 if __name__ == '__main__':
-    app.run(
-        debug=True
-    )
+    app.run(debug=True)
